a4b2fbdf59e49462d7edb3f7e0c14dc33a8573ff
t3chy <elam.day.friedland@gmail.com>
2020-07-06 08:53:22 -0700

    added weights (not working yet) and deprications

--- torch_ddpg.py
+++ torch_ddpg.py
@@ -5,19 +5,21 @@ import numpy as np
 env = gym.make('LunarLanderContinuous-v2')
 agent = Agent(alpha=0.000025,beta=0.00025, input_dims=[8],tau=0.001,env=env)
 np.random.seed(0)
+agent.load_models()
 score_history = []
 for i in range(1000):
     done = False
     score = 0
     obs = env.reset()
     while not done:
+        env.render()
         act = agent.choose_action(obs)
         new_state,reward,done,info = env.step(act)
         agent.remember(obs,act,reward,new_state,int(done))
         agent.learn()
         score += reward
         obs = new_state
-        score_history.append(score)
+    score_history.append(score)
     print('episode',i,'score%.2f' % score, '100 game average %.2f' % np.mean(score_history[-100:]))
     if i % 25 == 0:
         agent.save_models()    
